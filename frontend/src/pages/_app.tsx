1126c16298466418a63ec3971f9ff77d6504a113
Obinna Okolo <oojoseph67@gmail.com>
2023-07-21 01:32:34 +0100

    apollo frontend setup

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,23 +1,26 @@
-import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 import Head from "next/head";
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../chakra/theme';
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../chakra/theme";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../graphql/apollo-client";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider>
-      <Head>
-        <title>iMessage App</title>
-        <meta name="description" content="iMessage App by mcQu33n" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider>
+        <Head>
+          <title>iMessage App</title>
+          <meta name="description" content="iMessage App by mcQu33n" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
